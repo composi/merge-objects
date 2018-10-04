a050dba01aaafe745f1be4ff7e0c1600568a7e0e
Robert Biggs <rbiggs@ymail.com>
2018-10-03 22:08:12 -0700

    Refactored merge to allow deep clone.
    1. Previous version used JSON.stringify, etc. This cause some properties to not be copied over, such as function.
    2. This copies properties that are owned by the object. It does not copy properties on the prototype chain.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 /**
  * Combine two objects, merging the second into the first. Any properties already existing in the first will be replaced by those of the second. Any properties in the second not in the first will be added to it.
+ * This does a deep clone. Sub arrays will be cloned. If arrays consist of objects, those will be cloned. Functions will also be cloned.
+ * Passing in just one object will return a deep clone of it.
  *
  * @param {Object.<string, any>[]} objects
  * @return {Object.<string, any>} Object.<string, any>
  */
+
 export function mergeObjects(...objects) {
-  // Clone both objects:
-  const clones = objects.map(obj => JSON.parse(JSON.stringify(obj)))
-  // Merge objects:
-  return clones.reduce((a, b) => Object.assign({}, a, b))
+  // Add empty object to arguments.
+  // This insures the result is a clone.
+  objects.unshift({})
+
+  /**
+   * Merge on object into another.
+   * @param {Object<string, any>} target 
+   * @param {Object<string, any>} source
+   */
+  function createClone(target, source) {
+
+    for (let key in source) {
+      let descriptor = Object.getOwnPropertyDescriptor(source, key);
+      if (descriptor.value instanceof String) {
+        target[key] = new String(descriptor.value);
+      } else if (descriptor.value instanceof Array) {
+        target[key] = createClone([], descriptor.value);
+      } else if (descriptor.value instanceof Function) {
+        target[key] = descriptor.value
+      } else if (descriptor.value instanceof Object) {
+        let prototype = Reflect.getPrototypeOf(descriptor.value);
+        let clonedObject = createClone({}, descriptor.value);
+        Reflect.setPrototypeOf(clonedObject, prototype);
+        target[key] = clonedObject;
+      } else {
+        Object.defineProperty(target, key, descriptor);
+      }
+    }
+
+    let prototype = Reflect.getPrototypeOf(source);
+    Reflect.setPrototypeOf(target, prototype);
+    return target;
+  }
+  // Return cloned copy of merged objects:
+  return objects.reduce((a, b) => createClone(a, b))
 }
--- test/merge-objects.test.js
+++ test/merge-objects.test.js
@@ -29,9 +29,72 @@ test('merge should combine multiple objects into a new one', function () {
   expect(obj5.employer).toBe('Genentech')
 })
 
+test('should merge a function from one object to another', function() {
+  const obj1 = {name: 'Joe', job: 'mechanic'}
+  const obj2 = {name: 'Sam', announceName() {alert(`My name is ${this.name}`)}}
+  const obj3 = mergeObjects(obj1, obj2)
+  expect(obj3.name).toBe('Sam')
+  expect(obj3.job).toBe('mechanic')
+  expect(obj3).toHaveProperty('announceName')
+})
+
+test('should create a deep merge of objects', function() {
+  const obj1 = {
+    name: {
+      first: 'Joe'
+    }
+  }
+  const obj2 = {
+    name: {
+      first: 'Sam',
+      last: 'Smith'
+    },
+    stats: {
+      age: 32,
+      height: `5'10"`,
+      jobs: [
+        {
+          position: 'developer',
+          employer: 'Google',
+          status: 'former'
+        },
+        {
+          position: 'mechanic',
+          employer: 'Honda',
+          status: 'current'
+        }
+      ]
+    }
+  }
+  const obj3 = mergeObjects(obj1, obj2)
+  expect(obj3.name.first).toBe('Sam')
+  expect(obj3.name.last).toBe('Smith')
+  expect(obj3).toHaveProperty('stats')
+  expect(obj3.stats).toHaveProperty('jobs')
+  expect(obj3.stats.jobs[0].employer).toBe('Google')
+  expect(obj3.stats.jobs[1].employer).toBe('Honda')
+  obj2.stats.jobs[1].status = 'former'
+  expect(obj3.stats.jobs[1].status).toBe('current')
+})
+
 test("merge should create a clone of object.", function () {
   const obj1 = {name: 'Joe'}
   const obj2 = mergeObjects(obj1)
   expect(obj1 !== obj2).toBe(true)
   expect(obj1.name === obj2.name).toBe(true)
+})
+
+test('provding only one object should create a clone of it', function() {
+  const obj1 = {
+    name: {
+      first: 'Joe',
+      last: 'Bodoni'
+    }
+  }
+
+  const obj2 = mergeObjects(obj1)
+  expect(obj1 == obj2).toBe(false)
+  expect(obj1 === obj2).toBe(false)
+  obj1.name.last = 'Anderson'
+  expect(obj2.name.last).toBe('Bodoni')
 })
